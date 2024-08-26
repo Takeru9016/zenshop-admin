7349783a84cd753f9b55298682c3cda12c807b36
Takeru9016 <youthfox046@gmail.com>
2024-08-26 23:45:52 +0530

    feat: Initalized CockroachDB with Prisma ORM

--- src/components/modals/StoreModal.tsx
+++ src/components/modals/StoreModal.tsx
@@ -1,10 +1,42 @@
 "use client";
 
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
 import Modal from "../Modal";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
 import { useStoreModal } from "@/hooks/useStoreModal";
+
+const formSchema = z.object({
+  name: z.string().min(5),
+});
 
 export default function StoreModal() {
   const storeModal = useStoreModal();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+    {
+      /* TODO: CREATE SHOP/STORE */
+    }
+  };
 
   return (
     <Modal
@@ -13,7 +45,32 @@ export default function StoreModal() {
       isOpen={storeModal.isOpen}
       onClose={storeModal.onClose}
     >
-      Future Create Shop Form
+      <div className="space-y-4 py-2 pb-4">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="Enter shop name" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <div className="flex justify-end space-x-2 pt-6 items-center">
+              <Button variant="outline" onClick={storeModal.onClose}>
+                Cancel
+              </Button>
+              <Button type="submit">Continue</Button>
+            </div>
+          </form>
+        </Form>
+      </div>
     </Modal>
   );
 }
---
+++ src/lib/index.ts
@@ -0,0 +1,1 @@
+export { prismadb } from "./prismadb";
